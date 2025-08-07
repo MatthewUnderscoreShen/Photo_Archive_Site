8a5c713dc284fe8f4e1668d313220a4462db6052
MatthewUnderscoreShen <MatthewShen02@gmail.com>
2025-08-07 06:50:44 +0000

    implemented command line arg parsing and some other minor changes

--- img2thmb.py
+++ img2thmb.py
@@ -1,15 +1,34 @@
+# A script to take images and convert them to thumbnails
+# 
 from PIL import Image
-import os, glob
+import os
+import argparse
 
 archive_path = r"/home/matthew/pic_archive"
-years = ("2023", "2024", "2025")
-thmb_size = (100,100)
-for y in years:
-    year_path = os.path.join(archive_path, y)
-    thmb_year = "thmb" + y
-    for file in glob.glob(os.path.join(year_path, "*.JPG")):
+thumb_size = (100,100)
+
+# year: str of the folder name (a year)
+def generate_thumbnails(year):
+    year_path = os.path.join(archive_path, year)
+    thumb_year = "thumb" + year
+    try:
+        os.mkdir(thumb_year)
+    except FileExistsError:
+        pass
+    
+    for file in os.listdir(year_path):
         # file example: "/home/matthew/pic_archive/2025/DSC05986.JPG"
-        img = Image.open(file)
-        img.thumbnail(thmb_size)
-        thmb_path = os.path.join(archive_path, thmb_year, os.path.basename(file))
-        img.save(thmb_path)
+        full_path = os.path.join(year_path, file)
+        if os.path.isfile(full_path):
+            img = Image.open(full_path)
+            img.thumbnail(thumb_size)
+            thumb_path = os.path.join(archive_path, thumb_year, os.path.basename(full_path))
+            img.save(thumb_path)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("year", help="the year of images to create thumbnails of")
+    args = parser.parse_args()
+    years = args.year
+    for y in years:
+        generate_thumbnails(years)

a6cbcff02e99f316e6e56c4ae50962955219ad5f
MatthewUnderscoreShen <MatthewShen02@gmail.com>
2025-08-10 00:07:17 -0700

    changed absolute paths to relative

--- img2thmb.py
+++ img2thmb.py
@@ -4,30 +4,32 @@ from PIL import Image
 import os
 import argparse
 
-archive_path = r"/home/matthew/pic_archive"
-
 # year: str of the folder name (a year)
 def generate_thumbnails(year, size):
     thumb_size = (size, size)
-    year_path = os.path.join(archive_path, "images", year)
-    thumb_year = os.path.join(archive_path, "thumbnails", year)
+    image_year_dir = os.path.join("images", year)
+    thumb_year_dir = os.path.join("thumbnails", year)
     try:
-        os.mkdir(thumb_year)
+        os.mkdir(thumb_year_dir)
     except FileExistsError:
         pass
     
-    for file in os.listdir(year_path):
-        # file example: "/home/matthew/pic_archive/2025/DSC05986.JPG"
-        full_path = os.path.join(year_path, file)
-        if os.path.isfile(full_path):
-            img = Image.open(full_path)
-            img.thumbnail(thumb_size)
-            thumb_path = os.path.join(thumb_year, os.path.basename(full_path))
-            img.save(thumb_path)
+    for file in os.listdir(image_year_dir):
+        # file example: "/home/matthew/pic_archive/images/2025/DSC05986.JPG"
+        image_full_path = os.path.join(image_year_dir, file)
+        if not os.path.isfile(image_full_path):
+            continue
+        
+        img = Image.open(image_full_path)
+        img.thumbnail(thumb_size)
+        thumb_full_path = os.path.join(thumb_year_dir, os.path.basename(image_full_path))
+        img.save(thumb_full_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("year", help="the year of images to create thumbnails of")
-    parser.add_argument("size", type=int, help="size of the thumbnail (square)")
+    parser.add_argument("size", type=int, help="size of the thumbnail (square)",
+                        default=150)
     args = parser.parse_args()
     generate_thumbnails(args.year, args.size)
+    print(args.year + " done")
